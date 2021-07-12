3c6edfb7ededb1ebf8351953a661f1ffe5fd5a75
Omar Santiago <omarkenosia@gmail.com>
2021-07-12 17:44:14 -0400

    Add files via upload

---
+++ GroundEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; //for Editor
+
+// IMPORTANT!!! place script in Editor folder
+
+[CustomEditor(typeof(GroundScript))]
+public class GroundEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GroundScript script = (GroundScript)target;
+        if (GUILayout.Button("Spawn My Prefab") == true)
+        {
+            script.SpawnPrefab();
+        }
+    }
+}
---
+++ GroundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundScript : MonoBehaviour
+{
+    public GameObject PrefabObject = null;
+
+    public void SpawnPrefab()
+    {
+        //position over ground
+        Renderer rGrd = this.GetComponent<Renderer>();
+        Renderer rPrefab = PrefabObject.GetComponent<Renderer>();
+        float X = Random.Range(rGrd.bounds.min.x + rPrefab.bounds.extents.x, rGrd.bounds.max.x - rPrefab.bounds.extents.x);
+        float Y = rGrd.bounds.max.y + rPrefab.bounds.extents.y;
+        float Z = Random.Range(rGrd.bounds.min.z + rPrefab.bounds.extents.z, rGrd.bounds.max.z - rPrefab.bounds.extents.z);
+
+        //create prefab in scene
+        GameObject box = Instantiate(PrefabObject, new Vector3(X, Y, Z), Quaternion.identity);
+        box.GetComponent<Renderer>().material.color = Random.ColorHSV(); //rnadom color
+    }
+}
